956734de99530a1e1061952e0afbbdd19bd3dd4a
333NNN <nata.cunha@aluno.senai.br>
2025-02-26 19:38:00 -0300

    json e criação de arquivos

---
+++ 03_sistema_de_arquivos/lendoArquivo.js
@@ -0,0 +1,17 @@
+import { promises as fs } from "fs";
+import path from "path";
+import url from "url";
+
+var __filename = url.fileURLToPath(import.meta.url);
+var __dirname = path.dirname(__filename);
+
+var arquivo = path.join(__dirname, "totalVendas.json");
+
+try {
+  var buffer = await fs.readFile(arquivo);
+  console.log(buffer);
+  console.log(buffer.toString());
+  console.log(JSON.parse(buffer));
+} catch (error) {
+  console.error("Erro ao ler arquivo:", error);
+}
