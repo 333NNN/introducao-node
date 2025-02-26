2b104e9a4acb4c5bc2c8096d9fefb87c8f16706a
333NNN <nata.cunha@aluno.senai.br>
2025-02-26 19:08:54 -0300

    javascript

---
+++ 03_sistema_de_arquivos/criandoPasta.js
@@ -0,0 +1,17 @@
+import { promises as fs } from "fs";
+import path from "path";
+import url from "url";
+
+var __filename = url.fileURLToPath(import.meta.url);
+var __dirname = path.dirname(__filename);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+console.log(path.join(__dirname, "lojas", "202", "funcionarios"));
+
+try {
+  await fs.mkdir(path.join(__dirname, "lojas", "202", "funcionarios"));
+} catch (error) {
+  console.error(`Erro ao criar diretório, código: ${error.code}`);
+}
